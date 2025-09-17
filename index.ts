9e16d1bb455f28d5bf79aef781edc62fdb26b1f2
tnaomi <naomikapengule@gmail.com>
2025-09-17 18:56:30 +0200

    Created a code review agent

--- index.ts
+++ index.ts
@@ -1,9 +1,25 @@
-import { generateText } from "ai";
+import { stepCountIs, streamText } from "ai";
 import { google } from "@ai-sdk/google";
+import { SYSTEM_PROMPT } from "./prompts";
+import { getFileChangesInDirectoryTool } from "./tools";
 
-const { text } = await generateText({
-  model: google("models/gemini-2.5-flash"),   
-  prompt: "What is an AI agent?", 
-}); //Uses GOOGLE_GENERATIVE_AI_API_KEY environment variable
+const codeReviewAgent = async (prompt: string) => {
+  const result = streamText({
+    model: google("models/gemini-2.5-flash"),
+    prompt,
+    system: SYSTEM_PROMPT,
+    tools: {
+      getFileChangesInDirectoryTool: getFileChangesInDirectoryTool,
+    },
+    stopWhen: stepCountIs(10),
+  });
 
-console.log(text); 
+  for await (const chunk of result.textStream) {
+    process.stdout.write(chunk);
+  }
+};
+
+// Specify which directory the code review agent should review changes in your prompt
+await codeReviewAgent(
+  "Review the code changes in '../my-agent' directory, make your reviews and suggestions file by file",
+);
